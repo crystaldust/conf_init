46a997e507585c48f689b58da52b131fdd44c924
fivestarsky <fivestarsky@gmail.com>
2021-12-10 21:01:58 +0800

    init code

---
+++ main.py
@@ -0,0 +1,4 @@
+import init_dynaconf_config
+
+if __name__ == '__main__':
+    init_dynaconf_config.init_all_dynaconf_redis_config()
---
+++ settings.py
@@ -0,0 +1,18 @@
+REDIS_ENABLED_FOR_DYNACONF=True
+
+# REDIS_FOR_DYNACONF = {
+#     "host": "192.168.8.10",
+#     "port": 6379,
+#     "db": 0,
+#     "decode_responses": True,
+#     "password": "l4MdxZvlj7",
+# }
+
+# REDIS_HOST_FOR_DYNACONF=192.168.8.10
+# REDIS_PORT_FOR_DYNACONF=6379
+# REDIS_DB_FOR_DYNACONF=0
+# REDIS_DECODE_FOR_DYNACONF=True
+# REDIS_PASSWORD_FOR_DYNACONF=l4MdxZvlj7
+
+
+
